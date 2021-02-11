8052791cb0c402d0704f404411e47f1c8ce04b6d
Santiago Paladines <slpaladines@gmail.com>
2021-02-10 20:29:06 -0500

    Componentes
    Creación e importación de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BarraNavComponent } from './barra-nav/barra-nav.component';
+import { C2Component } from './c2/c2.component';
+import { FooterComponent } from './footer/footer.component';
+import { GalSerComponent } from './gal-ser/gal-ser.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarraNavComponent,
+    C2Component,
+    FooterComponent,
+    GalSerComponent
   ],
   imports: [
     BrowserModule,
