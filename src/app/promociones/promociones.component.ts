99c898983c228e8e6800110db73ffb13c7707d04
Johny-Bautista <jsbautista@uce.edu.ec>
2021-02-22 23:25:29 -0500

    Ofertas

--- src/app/promociones/promociones.component.ts
+++ src/app/promociones/promociones.component.ts
@@ -8,8 +8,24 @@ import { Component, OnInit } from '@angular/core';
 export class PromocionesComponent implements OnInit {
 
   constructor() { }
-
+  moferta = true;
+  ofnumero = 0;
   ngOnInit(): void {
   }
-
+  // tslint:disable-next-line:typedef
+  MostrarOferta($event){
+    this.moferta = ! this.moferta;
+  }
+  // tslint:disable-next-line:typedef
+  compraof1($event){
+    this.ofnumero = 1;
+  }
+  // tslint:disable-next-line:typedef
+  compraof2($event){
+    this.ofnumero = 2;
+  }
+  // tslint:disable-next-line:typedef
+  compraof3($event){
+    this.ofnumero = 3;
+  }
 }
